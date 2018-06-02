8d3cb561fecfd715ae3532c50fe2caca35c7bee7
Jose Perez <joseept27@gmail.com>
2018-06-02 09:13:09 -0400

    apollo removed and axios added

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,18 @@
+import axios from 'axios'
 import List from '../components/List'
 
-export default () => (
+const Index = ({ repos }) => (
   <List
-    data={[
-      {
-        name: 'Insurance Manager'
-      },
-      {
-        name: 'Insurance Manager'
-      },
-      {
-        name: 'Insurance Manager'
-      },
-      {
-        name: 'Insurance Manager'
-      },
-      {
-        name: 'Insurance Manager'
-      },
-      {
-        name: 'Insurance Manager'
-      },
-      {
-        name: 'Insurance Manager'
-      },
-      {
-        name: 'Insurance Manager'
-      },
-      {
-        name: 'Insurance Manager'
-      },
-      {
-        name: 'Insurance Manager'
-      },
-      {
-        name: 'Insurance Manager'
-      },
-      {
-        name: 'Insurance Manager'
-      },
-      {
-        name: 'Insurance Manager'
-      },
-      {
-        name: 'Insurance Manager'
-      },
-      {
-        name: 'Insurance Manager'
-      },
-      {
-        name: 'Insurance Manager'
-      },
-      {
-        name: 'Insurance Manager'
-      }
-    ]}
+    data={repos}
   />
 )
+
+Index.getInitialProps = async () => {
+  const { data } = await axios.get('https://api.github.com/users/josegpt/repos')
+
+  return {
+    repos: data
+  }
+}
+
+export default Index
